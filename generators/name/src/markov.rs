2b0872fdae5c0e4a74095cc37d5879b5e43c34de
Benedikt Schäfer <benediktschaefer@protonmail.com>
2025-10-12 21:27:25 +0200

    broken bigram state

--- generators/name/src/markov.rs
+++ generators/name/src/markov.rs
@@ -1,73 +1,78 @@
-use rand::{rngs::StdRng, SeedableRng};
+use std::collections::HashMap;
 
-const CHARACTERS: &str = "\0\x01abcdefghijklmnopqrstuvwxyzäöüß-"; // \0 is the "ending" character, \1 is the starting character
-const CHAR_COUNT: usize = 33;
+use rand::{Rng, SeedableRng, rngs::StdRng};
 
-struct MarkovModel<const RETAIN: usize> {
-    probabilities: [[f32; CHAR_COUNT]; CHAR_COUNT], // "None", abcdefg... see above
-    last_n : [char;RETAIN],                                 // array of the last used characters of size RETAIN
-    last_idx: usize,
+const ALLOWED_CHARACTERS: &str = "abcdefghijklmnopqrstuvwxyzäöüß-"; // \0 is the "ending" character, \1 is the starting character
+
+struct MarkovModel<const NGRAM: usize> {
+    ngrams: Vec<String>,
+    probabilities: Vec<Vec<f32>>,
+    last_ngram: String,
+    first_char: bool,
     rng: StdRng,
 }
 
-fn index(c: char) -> usize {
-    match c {
-        '\0' => 0,
-        '\x01' => 1,
-        'a'..='z' => (c as u32 - 0x61) as usize + 2,
-        'ä' => CHAR_COUNT - 5,
-        'ö' => CHAR_COUNT - 4,
-        'ü' => CHAR_COUNT - 3,
-        'ß' => CHAR_COUNT - 2,
-        '-' => CHAR_COUNT - 1,
-        _ => panic!("Illegal Character found: `{}` ({})", c, c.escape_debug()),
-    }
-}
-
-impl<const RETAIN: usize> MarkovModel<RETAIN> {
+impl MarkovModel<2> {
     fn new(input: &str) -> Self {
-        assert!(RETAIN > 0, "Must retain more than 0");
-        assert!(RETAIN < 4, "T = (m*RETAIN)x(m*RETAIN), please don't make your memory explode");
+        let mut ngrams = vec![];
+        let mut occurrences: Vec<Vec<u32>> = vec![];
 
-        let mut occ = [[0usize; CHAR_COUNT]; CHAR_COUNT];
-        occ[index('\0')][index('\0')] = 1;
         for line in input.split_whitespace() {
-            let mut previous_char = None;
-            for c in line.chars() {
-                match previous_char {
-                    None => occ[index('\x01')][index(c)] += 1,       // this is a character that a name started with
-                    Some(pc) => occ[index(pc)][index(c)] += 1,
-                }
-                previous_char = Some(c);
-            }
-            // the word is done, and the last char is followed by a terminator
-            occ[index(previous_char.unwrap())][index('\0')] += 1;
-        }
+            let mut previous_bigram: Option<[char; 2]> = None;
+            let mut citer = line.chars();
+            let mut current_bigram = ['\0', citer.next().unwrap()];
+            for c in citer {
+                current_bigram[0] = current_bigram[1];
+                current_bigram[1] = c;
+                // println!("{:?}", current_bigram);
 
-        let mut probabilities: [[f32;CHAR_COUNT];CHAR_COUNT] = occ
-            .iter()
-            .map(|&array| {
-                let sum = array.into_iter().fold(0, std::ops::Add::add);
-                match sum{
-                    0 => array.iter().map(|_| 1./CHAR_COUNT as f32).collect::<Vec<f32>>().try_into().unwrap(), // with 0 it is equally distributed
-                    _ => array
-                    .iter()
-                    .map(|&char_occ| char_occ as f32 / sum as f32)
-                    .collect::<Vec<f32>>()
-                    .try_into().unwrap()
+                match previous_bigram {
+                    None => (),
+                    Some(x) => {
+                        let src_ngram_idx =
+                            if let Some(ngram_idx) = ngrams.iter().position(|f| *f == x) {
+                                ngram_idx
+                            } else {
+                                ngrams.push(x.clone());
+                                occurrences.push(vec![]);
+                                ngrams.len() - 1
+                            };
+                        let dst_ngram_idx = if let Some(ngram_idx) =
+                            ngrams.iter().position(|f| *f == current_bigram)
+                        {
+                            ngram_idx
+                        } else {
+                            ngrams.push(x.clone());
+                            occurrences.push(vec![]);
+                            ngrams.len() - 1
+                        };
+                        if occurrences[src_ngram_idx].len() <= dst_ngram_idx {
+                            occurrences[src_ngram_idx].resize(dst_ngram_idx + 1, 0);
+                        }
+                        occurrences[src_ngram_idx][dst_ngram_idx] += 1;
+                    }
                 }
+                previous_bigram = Some(current_bigram);
+            }
+        }
+        let probabilities: Vec<Vec<f32>> = occurrences
+            .iter()
+            .map(|array| {
+                let sum = array.iter().fold(0, std::ops::Add::add);
+                println!("sum: {}", sum);
+                array.iter().map(|&el| el as f32 / sum as f32).collect()
             })
-            .collect::<Vec<_>>()
-            .try_into().unwrap();
-        probabilities[index('\0')][index('\0')] = 1.;
+            .collect();
+
         // println!("");
-        // for c in CHARACTERS.chars() {
-        //     println!("{}, `{c}` {:.2?}",index(c), probabilities[index(c)]);
+        // for c in ngrams.iter() {
+        //     println!("`{}{}`, {:.2?}", c[0], c[1], probabilities[ngrams.iter().position(|f| *f == *c).unwrap()]);
         // }
         Self {
+            ngrams: ngrams.iter().map(|x| String::from_iter(x.iter())).collect(),
             probabilities,
-            last_n: ['\x01';RETAIN], // the last used character was the start meta-char
-            last_idx: 0,
+            last_ngram: String::new(),
+            first_char: true,
             rng: StdRng::from_os_rng(),
         }
     }
@@ -76,48 +81,52 @@ impl<const RETAIN: usize> MarkovModel<RETAIN> {
 impl<const RETAIN: usize> Iterator for MarkovModel<RETAIN> {
     type Item = char;
     fn next(&mut self) -> Option<Self::Item> {
-        let last_used_character = self.last_n[self.last_idx];
-        self.last_idx = (self.last_idx+1) % RETAIN;
+        if self.last_ngram.is_empty() {
+            // initialize with a random bigram. the next iteration will look back on the chosen bigram and return a fitting one
+            self.last_ngram = self.ngrams[self.rng.random_range(0..self.ngrams.len())].clone();
+            println!("First Bigram: {}", self.last_ngram);
 
-        let mut idx = index('\0');
-        while idx == index('\0') {
-            idx = rand::seq::index::sample_weighted(&mut self.rng, CHAR_COUNT, |i| self.probabilities[index(last_used_character)][i], 1)
-            .unwrap().index(0);
+            return Some(self.last_ngram.chars().nth(0).unwrap());
+        } else if self.first_char {
+            self.first_char = false;
+            return Some(self.last_ngram.chars().nth(1).unwrap());
         }
-        let result = CHARACTERS.chars().nth(idx);
 
-        self.last_n[self.last_idx] = result.unwrap();
-        return result;
+        let src_idx = self
+            .ngrams
+            .iter()
+            .position(|f| *f == self.last_ngram)
+            .unwrap();
+        let idx = rand::seq::index::sample_weighted(
+            &mut self.rng,
+            self.probabilities[src_idx].len(),
+            |i| self.probabilities[src_idx][i],
+            1,
+        )
+        .unwrap()
+        .index(0);
+
+        println!(
+            "Last Bigram: `{}`@{:3?}, next one: `{}`@{:3?}",
+            self.last_ngram, src_idx, self.ngrams[idx], idx
+        );
+        self.last_ngram = self.ngrams[idx].clone();
+        // self.last_n = format!["{}{}", self.ngrams[src_idx].chars().nth(1).unwrap(), self.ngrams[idx].chars().nth(1).unwrap()];
+
+        return Some(self.ngrams[idx].chars().nth(1).unwrap());
     }
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use crate::markov::MarkovModel;
-    use super::{index};
 
     #[test]
-    fn index_function(){
-        for (i, c) in super::CHARACTERS.chars().enumerate() {
-            assert_eq!(i, index(c), "character `{c}` was at index {i} but index(c) = {}", index(c));
-        }
-    }
+    fn generate_10_ret1() {
+        let mut mkv: MarkovModel<2> = MarkovModel::new(include_str!("../names.txt"));
+        println!("markov bigrams: {}", mkv.ngrams.len());
+        todo!();
 
-    #[test]
-    fn assert_stochastic_mat(){
-        let mkv: MarkovModel<1> = MarkovModel::new(include_str!("../names.txt"));
-        for (i,l) in mkv.probabilities.iter().enumerate(){
-            let sum = l.into_iter().fold(0., |x,y| x+y);
-            println!("Error: {}", sum-1.);
-            assert!(sum - 1. < f32::EPSILON*10.,
-                "Failed in row {}: {:.2?} with a row sum of {}", i, l, sum
-            );
-        }
-    }
-
-    #[test]
-    fn generate_10_ret1(){
-        let mut mkv: MarkovModel<1> = MarkovModel::new(include_str!("../names.txt"));
         let mut s = String::new();
         for _ in 0..8 {
             s.push_str(&mkv.next().unwrap().to_string());
--- generators/name/src/matrix.rs
+++ generators/name/src/matrix.rs
@@ -1,16 +1,15 @@
-
-/// assumes row-major order: M = 
+/// assumes row-major order: M =
 /// [a,b,c],
 /// [d,e,f],
 /// [g,h,i]
 /// and M[0] = [a,b,c] NOT [a,d,g]
 /// also assumes no NaN / inf or that the user doesn't care
-fn mmul<const N: usize>(mi0: &[[f32;N];N], mi1: &[[f32;N];N]) -> [[f32;N];N] {
-    let mut mo = [[0.;N];N];
+fn mmul<const N: usize>(mi0: &[[f32; N]; N], mi1: &[[f32; N]; N]) -> [[f32; N]; N] {
+    let mut mo = [[0.; N]; N];
 
-    for i in 0..N{
-        for j in 0..N{
-            for k in 0..N{
+    for i in 0..N {
+        for j in 0..N {
+            for k in 0..N {
                 mo[i][j] += mi0[i][k] * mi1[k][j];
             }
         }
@@ -18,48 +17,51 @@ fn mmul<const N: usize>(mi0: &[[f32;N];N], mi1: &[[f32;N];N]) -> [[f32;N];N] {
     return mo;
 }
 
-fn id<const N: usize>() -> [[f32;N];N]{
-    let mut id = [[0.;N];N];
-    for i in 0..N{
+fn id<const N: usize>() -> [[f32; N]; N] {
+    let mut id = [[0.; N]; N];
+    for i in 0..N {
         id[i][i] = 1.;
     }
     id
 }
 
-fn mpow<const N: usize>(mi: &[[f32;N];N], n: usize) -> [[f32;N];N] {
-    if n == 0{
+fn mpow<const N: usize>(mi: &[[f32; N]; N], n: usize) -> [[f32; N]; N] {
+    if n == 0 {
         return id();
     }
-    if n == 1{
+    if n == 1 {
         return *mi;
     }
     let mut M = mi.clone();
 
-    for _ in 0..(n-1) {
+    for _ in 0..(n - 1) {
         M = mmul(&M, mi);
     }
     return M;
 }
 
-
 fn argmaxf32(slice: &[f32]) -> Option<usize> {
-    slice.iter().enumerate().max_by(|(_, value0), (_, value1)| value0.total_cmp(value1)).map(|x| x.0)
+    slice
+        .iter()
+        .enumerate()
+        .max_by(|(_, value0), (_, value1)| value0.total_cmp(value1))
+        .map(|x| x.0)
 }
 
 #[cfg(test)]
-mod testing{
-    use super::{mpow, id};
+mod testing {
+    use super::{id, mpow};
 
     #[test]
-    fn test_mpow(){
+    fn test_mpow() {
         let identity = id::<32>();
         let rot_z_pi_half = [[0f32, -1., 0.], [1., 0., 0.], [0., 0., 1.]];
 
-        for i in 0..4{
+        for i in 0..4 {
             assert_eq!(mpow(&identity, i), identity);
         }
         assert_eq!(mpow(&rot_z_pi_half, 0), id());
-        
+
         assert_eq!(mpow(&rot_z_pi_half, 4), id());
     }
 
